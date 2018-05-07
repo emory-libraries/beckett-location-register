c7ba58ce0e57d043f749aa706b6860276b272f7b
Lauren Hamel <lauren.hamel@outlook.com>
2018-05-07 12:57:13 -0400

    Adds `vendor` to gitignore

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitbf8252cc6975cb995d5a2cb5ce1b3eb8
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitbf8252cc6975cb995d5a2cb5ce1b3eb8::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirebf8252cc6975cb995d5a2cb5ce1b3eb8($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequirebf8252cc6975cb995d5a2cb5ce1b3eb8($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
