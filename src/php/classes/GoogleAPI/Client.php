eeb96a346eff297eb3c5bdfbaa9973b2589bc520
Lauren Hamel <lauren.hamel@outlook.com>
2018-09-06 09:10:20 -0400

    Disables Google API cache

--- src/php/classes/GoogleAPI/Client.php
+++ src/php/classes/GoogleAPI/Client.php
@@ -7,7 +7,9 @@ class Client {
   function __construct() {
     
     // Initialize the client.
-    $this->client = new Google_Client();
+    $this->client = new Google_Client([
+      'cache_class' => 'Google_Cache_Null'
+    ]);
     
     // Set the application name.
     $this->client->setApplicationName( $_ENV['APP_NAME'] );
